d479da1ba693e84351eaf8faa1f454fc8fe84dca
HrishilMaliwal <72825538+HrishilMaliwal@users.noreply.github.com>
2021-03-11 15:59:46 +0530

    Update Hrishil_Testing.py

--- Hrishil_Testing.py
+++ Hrishil_Testing.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
+import math
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
+
 
 df = pd.read_csv("stockdata.csv")
 df = df[2:]
@@ -40,7 +44,22 @@ X_test, y_test = create_dataset(test_data, time_step)
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[0], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(100, 1)))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
 
+model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=100,batch_size=64,verbose=1)
+
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+
+train_predict=scaler.inverse_transform(train_predict)
+test_predict=scaler.inverse_transform(test_predict)
+
+math.sqrt(mean_squared_error(y_train,train_predict))
+math.sqrt(mean_squared_error(ytest,test_predict))
 
 
 # print(close_data)
