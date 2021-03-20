d7bc798125ab7886dbce71922827d418f5f42e7d
madiha2001 <madiha.mansoori2001@gmail.com>
2021-03-20 18:40:24 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,10 +1,16 @@
 import yfinance as yf
 import pandas as pd
+ticker='xyz'
+
+
 df=pd.read_html('https://in.finance.yahoo.com/quote/%5ENSEI/components?p=%5ENSEI&.tsrc=fin-srch')[0]
 tickers=df.Symbol.to_list()
 print(tickers)
-ticker='xyz'
 while ticker not in tickers:
   ticker=input("choose one from the list:")
-df=yf.download(ticker)
-print(df)
+
+def get_data(ticktick):
+    df1=yf.download(ticker)
+    print(df1)
+    #return(df1)
+get_data(ticker)
